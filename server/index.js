3b36147054f9bb363d1f9ad49caa2eaa260e411c
AbdulRazak Abubakari <abdulrazakneate@gmail.com>
2022-06-01 13:30:13 +0000

    add  cors options back

--- server/index.js
+++ server/index.js
@@ -10,13 +10,13 @@ const whitelist = ['http://localhost:5000', 'http://localhost:8080', 'https://ca
 
 const corsOptions = {
     origin: function (origin, callback) {
-      console.log("** Origin of request " + origin)
+      console.log("**  Origin of request " + origin)
       if (whitelist.indexOf(origin) !== -1 || !origin) {
         console.log("Origin acceptable")
         callback(null, true);
 
       } else {
-        console.log("Origin rejected")
+        console.log("Origin rejected") 
         callback(new Error('Not allowed by CORS'))
       }
     }
